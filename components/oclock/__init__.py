443cf68461d4e87d02cb83c2cf07713f48d89437
Henri <h.vd.esker@gmail.com>
2021-10-06 12:08:03 +0200

    update

--- components/oclock/__init__.py
+++ components/oclock/__init__.py
@@ -1,54 +1,56 @@
 # depending on https://materialdesignicons.com/
-from logging import Logger
-import logging
-from os import defpath
-from typing import Optional
-from esphome.components.font import (
-    CONF_RAW_DATA_ID,
-    CONF_RAW_GLYPH_ID
-)
-from esphome.const import CONF_BAUD_RATE, CONF_ID, CONF_LOGGER
+from esphome.const import CONF_BAUD_RATE, CONF_DEVICE_CLASS, CONF_DISABLED_BY_DEFAULT, CONF_ID, CONF_LOGGER, CONF_NAME
 from esphome.core import CORE
-import importlib
-from esphome.cpp_generator import MockObj, Pvariable
-from esphome.helpers import read_file
-from esphome.voluptuous_schema import _Schema
 from esphome import core
-from pathlib import Path
+from esphome.voluptuous_schema import _Schema
+from esphome.components import time, switch
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import loader
-import sys
-import json
+from esphome.cpp_generator import MockObj, Pvariable
+CONF_TIME_ID = "time_id"
 
-source_files = ["hoi.c"]
+
+DEPENDENCIES = ["time", "switch"]
+CODEOWNERS = ["@hvandenesker"]
+
 
 oclock_ns = cg.esphome_ns.namespace("oclock_stub")
 
 OClockStubController = oclock_ns.class_("OClockStubController", cg.Component)
 
-
-_LOGGER = logging.getLogger(__name__)
-
-
 CONFIG_SCHEMA = _Schema(
     {
         cv.GenerateID(): cv.declare_id(OClockStubController),
+        cv.Optional(CONF_TIME_ID): cv.use_id(time.RealTimeClock)
     }
 )
 
 
-
-DEPENDENCIES = []
-CODEOWNERS = ["@hvandenesker"]
-
-
 async def to_code(config):
+    # https://github.com/esphome/AsyncTCP/blob/master/library.json
+    cg.add_library(name="apa102-arduino", version=None,
+                   repository="pluggerz/apa102-arduino.git")
+
     if CONF_LOGGER in CORE.config:
-        baudrate=CORE.config[CONF_LOGGER][CONF_BAUD_RATE]
+        baudrate = CORE.config[CONF_LOGGER][CONF_BAUD_RATE]
         if (baudrate != 0):
             raise cv.Invalid(f"Make sure logger.baudrate = 0 !")
 
     var = cg.new_Pvariable(config[CONF_ID])
+    if CONF_TIME_ID in config:
+        cg.add(var.set_time(await cg.get_variable(config[CONF_TIME_ID])))
+
+    resetSwitchId = core.ID(
+        "oclock_reset", is_declaration=False, type=switch.Switch)
+
+    resetSwitchConf = {
+        CONF_ID: resetSwitchId,
+        CONF_NAME: str(resetSwitchId),
+        CONF_DISABLED_BY_DEFAULT: False,
+        # CONF_DEVICE_CLASS: device_class,
+    }
+    resetSensorVar: Pvariable = cg.Pvariable(
+        resetSwitchConf[CONF_ID], cg.RawExpression("new oclock::ResetSwitch()"))
+    await switch.register_switch(resetSensorVar, resetSwitchConf)
+
     await cg.register_component(var, config)
-    
---
+++ components/oclock/interop.md
@@ -0,0 +1,13 @@
+Master/Slave put OUT initial LOW
+
+Slave:
+IN | OUT
+L L receive
+H L receive
+H H receive
+L H N/A
+
+Master:  PING(from:master, slaveId=0)
+Slave0:  
+
+
