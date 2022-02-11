ba898c5195fa36859258acf8f48797f6d4f99174
Henri <h.vd.esker@gmail.com>
2022-02-11 18:40:41 +0100

    add button

--- components/oclock/__init__.py
+++ components/oclock/__init__.py
@@ -208,6 +208,7 @@ async def to_code(config):
     await cg_add_switch("oclock_dump_config", cg.RawExpression("new oclock::DumpConfigSwitch()"))
     await cg_add_switch("oclock_dump_config_slaves", cg.RawExpression("new oclock::DumpConfigSlavesSwitch()"))
 
+    cg.add(cg.RawExpression("new oclock::ZeroPosition();"));
     cg.add(cg.RawExpression("new oclock::TurnSpeedControl();"));
     cg.add(cg.RawExpression("new oclock::TurnStepsControl();"));
     cg.add(cg.RawExpression("new oclock::ActiveModeSelect();"));
